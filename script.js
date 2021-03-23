de283b94e7376d79a80a11deac056435a97895c3
Smidish <s.hinterreiter@gmx.de>
2021-03-23 16:32:57 +0100

    page 1-3 done

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function play() {
             
         }
     }
+
+    function switchPic(){
+        var pic1 = document.getElementById('person_img');
+        var pic2 = document.getElementById('img_2');
+        var input = document.getElementById('answer');
+        var btn_next = document.getElementById('btn_riddle');
+        var btn_input = document.getElementById('btn_input');
+        var btn_play = document.getElementById('play');
+
+        pic1.style.display = "none";
+        btn_play.style.display = "none";
+        btn_next.style.display = "none";
+
+        pic2.style.display = "block";
+        input.style.display = "block";
+        btn_input.style.display = "block";
+    }
